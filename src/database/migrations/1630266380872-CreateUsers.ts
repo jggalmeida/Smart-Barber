9161a72f45a95ba0f3e2e06646350623d7fbb867
jggalmeida <j.g.almeida@hotmail.com>
2021-08-30 20:28:49 -0300

    Model User

--- src/database/migrations/1630266380872-CreateUsers.ts
+++ src/database/migrations/1630266380872-CreateUsers.ts
@@ -4,7 +4,7 @@ export default class CreateUsers1630266380872 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: 'appointments',
+        name: 'users',
         columns: [
           {
             name: 'id',
@@ -14,14 +14,30 @@ export default class CreateUsers1630266380872 implements MigrationInterface {
             default: 'uuid_generate_v4()',
           },
           {
-            name: 'provider',
+            name: 'name',
             type: 'varchar',
             isNullable: false,
           },
           {
-            name: 'date',
-            type: 'timestamp with time zone',
+            name: 'email',
+            type: 'varchar',
             isNullable: false,
+            isUnique: true,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
           },
         ],
       }),
@@ -29,6 +45,6 @@ export default class CreateUsers1630266380872 implements MigrationInterface {
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable('appointments');
+    await queryRunner.dropTable('users');
   }
 }
